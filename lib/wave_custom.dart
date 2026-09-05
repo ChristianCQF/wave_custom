fd479b6bbb6e0a1aeb07a1ffb8273c8a83263b0a
Christian CQF <quilcoflores@gmail.com>
2026-09-05 12:28:46 -0500

    chore: bump version to 0.0.2

--- lib/wave_custom.dart
+++ lib/wave_custom.dart
@@ -6,4 +6,11 @@ export 'src/widget/wave.dart';
 git add .
 git commit -m "feat: Actualizacion de nombres shape 2"
 git push origin main
+
+
+
+git add .
+git commit -m "chore: bump version to 0.0.2"
+git tag v0.0.2
+git push origin main --tags
 */
